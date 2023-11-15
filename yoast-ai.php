b24b9c08262e928ad3e678832165c304e6b7e97c
Ricky Clark III <Ricky.Clark@mines.sdsmt.edu>
2023-11-14 20:04:16 -0700

    Created header file

---
+++ yoast-ai.php
@@ -0,0 +1,13 @@
+/*
+ * Plugin Name:       Yoast AI
+ * Plugin URI:        https://github.com/Ricky-ClarkIII/Yoast-AI
+ * Description:       ChatGPT and Yoast SEO Integration for Optimal SEO
+ * Version:           1.0.0
+ * Requires PHP:      7.2
+ * Author:            John Smith
+ * Author URI:        https://github.com/Ricky-ClarkIII
+ * License:           GPL v3 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-3.0.en.html
+ * Update URI:        https://github.com/Ricky-ClarkIII/Yoast-AI
+ * Text Domain:       yoast-ai
+ */
